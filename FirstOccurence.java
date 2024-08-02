74742296c0b6808785b27e767ef312ec69e12ef9
Myo Nyi Nyi (Jeremy) <Jeremy.myo@attsystemsgroup.com>
2024-08-02 11:22:07 +0800

    solution to No. 28 Haystack & needle

--- FirstOccurence.java
+++ FirstOccurence.java
@@ -1,7 +1,37 @@
 class FirstOccurence {
 
+
 	public int strStr(String haystack, String needle) {
+		// haystack = "hihello" length => 7
+		// needle = "hello" length => 5
 
 		byte [] haystackArr = haystack.getBytes();
+		byte [] needleArr = needle.getBytes(); 
+
+		// for loop running from 0, 1 , 2 ( three times)
+		// running n times => number of items in the haystack
+		for (int i = 0; i <= haystackArr.length - needleArr.length; i++) {
+
+			// running m times => number of items in the needle
+			for (int j = 0; j < needleArr.length; j++) {
+				if (needleArr[j] != haystackArr[i + j]) {
+					break;
+				}
+
+				if (j == needleArr.length - 1 && needleArr[j] == haystackArr[i + j]) {
+					return i;
+				}
+			}
+			
+		}
+
+		// total O(n^2) ?
+		return -1;
+	}
+	
+	//Given answer is more straightforward; its still O(m*n) though
+
+	public int solution(String haystack, String needle) {
+		return haystack.indexOf(needle);
 	}
 }
