4634eb8bc69e8b8464ff48b9cf121203f0aa34e6
myonster <e0773753@u.nus.edu>
2024-08-07 17:07:33 +0800

    Set Matrix in Progress

---
+++ SetMatrixZeroes.java
@@ -0,0 +1,42 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class SetMatrixZeroes {
+	public int [][] setZeroes(int[][] matrix) {
+		boolean [] index = new boolean [matrix[0].length];
+		boolean [] rowIsZero = new boolean[matrix.length];
+
+		for (int j = 0; j < matrix.length; j++) {
+			//int [] rowCheck = matrix[j].clone();
+			boolean haveZero = false;
+			for (int i = 0; i < matrix[j].length; i++) {
+				if (matrix[j][i] == 0) {
+					//System.out.println("0 and not 0 from index");
+					haveZero = true;
+					index[i] = true;
+				}
+
+			if (haveZero) {
+				rowIsZero[j] = true;
+				matrix[j] = new int [matrix[j].length];
+			}
+				// if (index[i] > 0) {
+				// 	//System.out.println("index is 0, column affected!");
+				// 	matrix[j][i] = 0;
+				// }
+			}
+		}
+
+		for (int j = 0; j < matrix.length; j++) {
+			if (rowIsZero[j]) {
+				for (int i = 0; i < matrix[j].length; i++) {
+					if (index[i]) {
+						matrix[i][j] = 0;
+					}
+				}
+			}
+		}
+		
+		return matrix;
+	}
+}
