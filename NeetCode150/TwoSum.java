5212daa8fdd53c5ceb51598e55d28f8451055267
myonster <e0773753@u.nus.edu>
2024-09-02 18:44:55 +0800

    twosum

---
+++ NeetCode150/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
+class TwoSum {
+	public int [] twoSum(int [] nums, int target) {
+		Arrays.sort(nums);
+		int [] arr = new int [2];
+		int leftPointer = 0;
+		int rightPointer = nums.length - 1;
+
+		HashMap<Integer, Integer> indexMap = new HashMap<Integer,Integer>();
+
+		for (int i = 0; i < nums.length; i++) {
+			indexMap.put(nums[i], i);
+			int complement = target - nums[i];
+
+			if (indexMap.containsKey(complement)) {
+				arr[0] = i;
+				arr[1] = indexMap.get(complement);
+
+				return arr; 
+			}
+		}
+		
+		return	arr;
+	}
+}
